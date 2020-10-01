e54a9c03ebafb8b57461d2ee8f561bf32b9e0aff
unknown <sebalreves@gmail.com>
2020-10-01 02:01:36 -0300

    procesar solicitud avance request

--- src/components/ProcesarSolicitud/ProcesarSolicitud.js
+++ src/components/ProcesarSolicitud/ProcesarSolicitud.js
@@ -18,6 +18,7 @@ import {
   Input,
   CustomInput,
 } from "reactstrap";
+import { Typography } from "@material-ui/core";
 import ButtonMaterial from "@material-ui/core/Button";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
@@ -43,13 +44,38 @@ export default function ProcesarSolicitud(props) {
   const [comentario, setComentario] = useState("");
   const [imagen, setImagen] = useState(null);
   const [correos, setCorreos] = useState([""]);
+  const [submit, setSubmit] = useState(false);
 
-  const aprobar = () => {
-    console.log("aprobar");
-    console.log(imagen);
-  };
-  const rechazar = () => {
-    console.log("rechazar");
+  const enviar = (aprobar) => {
+    var respuesta = {
+      comentario: comentario,
+      imagen_complementaria: imagen,
+      correos: correos,
+    };
+    var link; //link + user + oid
+    if (aprobar) {
+      console.log("aprobar");
+      console.log(respuesta);
+    } else {
+      console.log("rechazar");
+    }
+
+    axios({
+      method: "post",
+      url: link,
+      data: "request=" + JSON.stringify(respuesta),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        setSubmit("ok");
+      })
+      .catch((err) => {
+        setSubmit("error");
+        console.log(err);
+      });
   };
 
   return (
@@ -65,56 +91,77 @@ export default function ProcesarSolicitud(props) {
                 "overflow-y": "scroll",
                 border: "0px",
               }}>
-              <Verificacion state={props.state} />
-              <legend>Correo adicional</legend>
-              <FormGroup>
-                <Col sm={10}>
-                  <Input
-                    className="observaciones"
-                    bsSize="sm"
-                    type="textarea"
-                    placeholder=""
-                    value={correos[0]}
-                    onChange={(e) => setCorreos([e.currentTarget.value])}
-                  />
-                </Col>
-              </FormGroup>
-              <legend>Imagen complementaria</legend>
-              <FormGroup>
-                <CustomInput
-                  className="boton-file"
-                  type="file"
-                  label={"file"}
-                  onChange={(e) => {
-                    setImagen(e.target.files[0]);
-                  }}
-                />
-              </FormGroup>
-              <legend>Comentarios adicionales</legend>
-              <FormGroup>
-                <Col sm={10}>
-                  <Input
-                    className="observaciones"
-                    bsSize="sm"
-                    type="textarea"
-                    placeholder=""
-                    value={comentario}
-                    onChange={(e) => setComentario(e.currentTarget.value)}
-                  />
-                </Col>
-              </FormGroup>
-              <ButtonMaterial
-                variant="contained"
-                color="primary"
-                onClick={aprobar}>
-                Aprobar
-              </ButtonMaterial>
-              <ButtonMaterial
-                variant="contained"
-                color="primary"
-                onClick={rechazar}>
-                Rechazar
-              </ButtonMaterial>
+              {submit === false ? (
+                <>
+                  <Verificacion state={props.state} />
+                  <hr className="separador"></hr>
+                  <h2>Procesar solicitud</h2>
+                  <legend>Imagen complementaria</legend>
+                  <FormGroup>
+                    <CustomInput
+                      className="boton-file"
+                      type="file"
+                      label={"file"}
+                      onChange={(e) => {
+                        setImagen(e.target.files[0]);
+                      }}
+                    />
+                  </FormGroup>
+                  <legend>Comentarios adicionales</legend>
+                  <FormGroup>
+                    <Col sm={10}>
+                      <Input
+                        className="observaciones"
+                        bsSize="sm"
+                        type="textarea"
+                        placeholder=""
+                        value={comentario}
+                        onChange={(e) => setComentario(e.currentTarget.value)}
+                      />
+                    </Col>
+                  </FormGroup>
+                  <legend>Correo adicional</legend>
+                  <FormGroup>
+                    <Col sm={10}>
+                      <Input
+                        className="observaciones"
+                        bsSize="sm"
+                        type="textarea"
+                        placeholder=""
+                        value={correos[0]}
+                        onChange={(e) => setCorreos([e.currentTarget.value])}
+                      />
+                    </Col>
+                  </FormGroup>
+                  <ButtonMaterial
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      enviar(true);
+                    }}>
+                    Aprobar
+                  </ButtonMaterial>
+                  <ButtonMaterial
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      enviar(false);
+                    }}>
+                    Rechazar
+                  </ButtonMaterial>
+                </>
+              ) : (
+                <>
+                  <Typography className={classes.instructions}>
+                    {submit === "ok"
+                      ? "Formulario enviado con exito"
+                      : "Error de envío del formulario, si el problema persiste contactar con el administrador"}
+                  </Typography>
+                  <Link to="/">
+                    <span>Volver al inicio</span>
+                  </Link>
+                </>
+              )}
             </div>
           </div>
         </div>
