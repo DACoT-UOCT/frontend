112b8a5a58942eb8b002a6f163f88820825e31dd
Pedro Arriagada <pedro.arriagadan@sansano.usm.cl>
2020-09-30 21:34:11 -0300

    Tabla lista en vista 1 formulario

--- src/components/shared/Campos/Cabezales.js
+++ src/components/shared/Campos/Cabezales.js
@@ -1,8 +1,20 @@
 import React, { useContext } from "react";
 
 import "../../../App.css";
-import { Col, FormGroup, Label } from "reactstrap";
-import TextField from "@material-ui/core/TextField";
+import { Label } from "reactstrap";
+import {  Checkbox,
+          FormControlLabel,
+          Table,
+          TableBody,
+          TableCell,
+          TableContainer,
+          TableRow,
+          TextField,
+          styled } from '@material-ui/core';
+
+const Campo = styled(TextField)({
+  background: "none",
+});
 
 const Cabezales = (props) => {
   const state = props.state;
@@ -10,251 +22,247 @@ const Cabezales = (props) => {
 
   return (
     <>
-      {console.log("render cabezales")}
       <legend className="seccion">Cabezales</legend>
-      <FormGroup row>
-        <Label sm={2}></Label>
-        <Label sm={2}>Halógeno</Label>
-        <Label sm={2}>Led</Label>
-      </FormGroup>
-      <FormGroup row>
-        <Label sm={2}>Vehiculo L1</Label>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l1-hal"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l1.hal}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l1",
-                fieldName: "hal",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l1-led"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l1.led}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l1",
-                fieldName: "led",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label sm={2}>Vehiculo L2</Label>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l2-hal"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l2.hal}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l2",
-                fieldName: "hal",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l2-led"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l2.led}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l2",
-                fieldName: "led",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label sm={2}>Vehiculo L3-L4</Label>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l3_l4-hal"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l3_l4.hal}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l3_l4",
-                fieldName: "hal",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l3_l4-led"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l3_l4.led}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l3_l4",
-                fieldName: "led",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label sm={2}>Vehiculo L5</Label>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l5-hal"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l5.hal}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l5",
-                fieldName: "hal",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l5-led"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l5.led}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l5",
-                fieldName: "led",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Col sm={2}>
-          <Label>Vehiculo L6</Label>
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l6-hal"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l6.hal}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l6",
-                fieldName: "hal",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-l6-led"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.l6.led}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.l6",
-                fieldName: "led",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Col sm={2}>
-          <Label>Peatonal</Label>
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-peatonal-hal"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.peatonal.hal}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.peatonal",
-                fieldName: "hal",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-        <Col sm={2}>
-          <TextField
-            id="outlined"
-            variant="outlined"
-            name="cabezales-peatonal-led"
-            type="number"
-            autoComplete="off"
-            style={{ width: "75px" }}
-            value={state.peatonal.led}
-            onChange={(e) => {
-              dispatch({
-                type: "cabezales.peatonal",
-                fieldName: "led",
-                payLoad: e.currentTarget.value,
-              });
-            }}
-          />
-        </Col>
-      </FormGroup>
+
+      <TableContainer>
+        <Table size="small" aria-label="simple table">
+          <TableBody>
+            <TableRow>
+              <TableCell component="th" scope="row"><Label></Label></TableCell>
+              <TableCell align="left"><Label className='inputCabezales'>Vehiculo L1</Label></TableCell>
+              <TableCell align="left"><Label className='inputCabezales'>Vehiculo L2</Label></TableCell>
+              <TableCell align="left"><Label className='inputCabezales'>Vehiculo L3-L4</Label></TableCell>
+              <TableCell align="left"><Label className='inputCabezales'>Vehiculo L5</Label></TableCell>
+              <TableCell align="left"><Label className='inputCabezales'>Vehiculo L6</Label></TableCell>
+              <TableCell align="left"><Label className='inputCabezales'>Peatonal</Label></TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row"><Label>Halógeno</Label></TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l1-hal"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l1.hal}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l1",
+                      fieldName: "hal",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                id="standard"
+                variant="standard"
+                name="cabezales-l2-hal"
+                type="number"
+                autoComplete="off"
+                style={{ width: "75px" }}
+                value={state.l2.hal}
+                onChange={(e) => {
+                  dispatch({
+                    type: "cabezales.l2",
+                    fieldName: "hal",
+                    payLoad: e.currentTarget.value,
+                  });
+                }}
+              />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l3_l4-hal"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l3_l4.hal}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l3_l4",
+                      fieldName: "hal",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l5-hal"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l5.hal}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l5",
+                      fieldName: "hal",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l6-hal"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l6.hal}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l6",
+                      fieldName: "hal",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-peatonal-hal"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.peatonal.hal}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.peatonal",
+                      fieldName: "hal",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row"><Label>Led</Label></TableCell>
+              <TableCell align="left">
+                <Campo
+                id="standard"
+                variant="standard"
+                name="cabezales-l1-led"
+                type="number"
+                autoComplete="off"
+                style={{ width: "75px" }}
+                value={state.l1.led}
+                onChange={(e) => {
+                  dispatch({
+                    type: "cabezales.l1",
+                    fieldName: "led",
+                    payLoad: e.currentTarget.value,
+                  });
+                }}
+              />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l2-led"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l2.led}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l2",
+                      fieldName: "led",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l3_l4-led"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l3_l4.led}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l3_l4",
+                      fieldName: "led",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l5-led"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l5.led}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l5",
+                      fieldName: "led",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-l6-led"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.l6.led}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.l6",
+                      fieldName: "led",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  variant="standard"
+                  name="cabezales-peatonal-led"
+                  type="number"
+                  autoComplete="off"
+                  style={{ width: "75px" }}
+                  value={state.peatonal.led}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "cabezales.peatonal",
+                      fieldName: "led",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
--- src/components/shared/Campos/UPS.js
+++ src/components/shared/Campos/UPS.js
@@ -4,8 +4,20 @@ import {
   StateContext,
 } from "../../SolicitudInstalacionNueva/NuevaInstalacion";
 import "../../../App.css";
-import { Col, Row, FormGroup } from "reactstrap";
-import TextField from "@material-ui/core/TextField";
+import { Label } from "reactstrap";
+import {  Checkbox,
+          FormControlLabel,
+          Table,
+          TableBody,
+          TableCell,
+          TableContainer,
+          TableRow,
+          TextField,
+          styled } from '@material-ui/core';
+
+const Campo = styled(TextField)({
+  background: "none",
+});
 
 const UPS = (props) => {
   const state = props.state;
@@ -14,109 +26,127 @@ const UPS = (props) => {
   return (
     <>
       <legend className="seccion">UPS</legend>
-      <Row form>
-        <Col sm={3}>
-          <FormGroup>
-            <TextField
-              id="outlined"
-              label="Marca"
-              variant="outlined"
-              name="ups-marca"
-              autoComplete="off"
-              value={state.marca}
-              onChange={(e) => {
-                dispatch({
-                  type: "ups",
-                  fieldName: "marca",
-                  payLoad: e.currentTarget.value,
-                });
-              }}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={1}></Col>
-        <Col sm={3}>
-          <FormGroup>
-            <TextField
-              id="outlined"
-              label="Modelo"
-              variant="outlined"
-              name="ups-modelo"
-              autoComplete="off"
-              value={state.modelo}
-              onChange={(e) => {
-                dispatch({
-                  type: "ups",
-                  fieldName: "modelo",
-                  payLoad: e.currentTarget.value,
-                });
-              }}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={1}></Col>
-        <Col sm={3}>
-          <FormGroup>
-            <TextField
-              id="outlined"
-              label="N° Serie"
-              variant="outlined"
-              name="ups-serie"
-              autoComplete="off"
-              value={state.n_serie}
-              onChange={(e) => {
-                dispatch({
-                  type: "ups",
-                  fieldName: "n_serie",
-                  payLoad: e.currentTarget.value,
-                });
-              }}
-            />
-          </FormGroup>
-        </Col>
-      </Row>
 
-      <Row form>
-        <Col sm={3}>
-          <FormGroup>
-            <TextField
-              id="outlined"
-              label="Capacidad"
-              variant="outlined"
-              name="ups-capacidad"
-              autoComplete="off"
-              value={state.capacidad}
-              onChange={(e) => {
-                dispatch({
-                  type: "ups",
-                  fieldName: "capacidad",
-                  payLoad: e.currentTarget.value,
-                });
-              }}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={1}></Col>
-        <Col sm={3}>
-          <FormGroup>
-            <TextField
-              id="outlined"
-              label="Duración Carga"
-              variant="outlined"
-              name="ups-duracion"
-              autoComplete="off"
-              value={state.duracion_carga}
-              onChange={(e) => {
-                dispatch({
-                  type: "ups",
-                  fieldName: "duracion_carga",
-                  payLoad: e.currentTarget.value,
-                });
-              }}
-            />
-          </FormGroup>
-        </Col>
-      </Row>
+      <TableContainer>
+        <Table size="small" aria-label="simple table">
+          <TableBody>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <Label>Marca</Label>
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  label="Marca"
+                  variant="standard"
+                  name="ups-marca"
+                  autoComplete="off"
+                  value={state.marca}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "ups",
+                      fieldName: "marca",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <Label>Modelo</Label>
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  label="Modelo"
+                  variant="standard"
+                  name="ups-modelo"
+                  autoComplete="off"
+                  value={state.modelo}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "ups",
+                      fieldName: "modelo",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <Label>N° Serie</Label>
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  label="N° Serie"
+                  variant="standard"
+                  name="ups-serie"
+                  autoComplete="off"
+                  value={state.n_serie}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "ups",
+                      fieldName: "n_serie",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <Label>Capacidad</Label>
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  label="Capacidad"
+                  variant="standard"
+                  name="ups-capacidad"
+                  autoComplete="off"
+                  value={state.capacidad}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "ups",
+                      fieldName: "capacidad",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <Label>Duración Carga</Label>
+              </TableCell>
+              <TableCell align="left">
+                <Campo
+                  id="standard"
+                  label="Duración Carga"
+                  variant="standard"
+                  name="ups-duracion"
+                  autoComplete="off"
+                  value={state.duracion_carga}
+                  onChange={(e) => {
+                    dispatch({
+                      type: "ups",
+                      fieldName: "duracion_carga",
+                      payLoad: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
